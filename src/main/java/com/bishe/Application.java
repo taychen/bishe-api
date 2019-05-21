030688b7177008ce9edf6b86792735d799b40c7d
陈泰 <chentay@aliyun,com>
2019-05-21 16:00:25 +0800

    初始化项目

---
+++ src/main/java/com/bishe/Application.java
@@ -0,0 +1,19 @@
+package com.bishe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目服务入口
+ *
+ * @author chentay
+ * @date 2019/05/21
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
